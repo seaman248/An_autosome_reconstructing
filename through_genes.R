7a75b741e216fe3a291a33d7cfa2c8580b2d11aa
seaman248 <semen.bko@ya.ru>
2017-01-23 15:41:41 +0700

    change concept from lapply to for cycle

--- through_genes.R
+++ through_genes.R
@@ -1,17 +1,19 @@
 atr_genes <-  cGeneTable %>%
   select(atr_ID, atr_scf, atr_start, atr_end, atr_strand)
+atr_genes$tStart <- atr_genes$tEnd <- atr_genes$tStrand <- atr_genes$chr <- rep(NA, nrow(atr_genes))
+scf_order <- read.csv2('./atr_order.csv')
 
-scf_order_data <- read.csv2('./atr_order.csv')
-
-# Определяем порядок хромосом
-chr_order <- list('2L', '2R', '3L', '3R')
-#
-scf_order <- list()
 # Делаем лист из data.frame'ов для каждой хромосомы 
 scf_order <- lapply(chr_order, function(i){
-  scf_order$i<-as.data.frame(scf_order_data[scf_order_data$chr==i,])
+  scf_order$i<-scf_order_data[scf_order_data$chr==i,]
 })
+names(scf_order) <- chr_order
 # Изначальная таблица больше не нужна
 rm(scf_order_data)
 
 # 
+
+
+for(i in 1:nrow(atr_genes)){
+  
+}
