fed3804f86b9457fe75040e65aea46bf143169a3
seaman248 <semen.bko@ya.ru>
2017-01-23 15:42:52 +0700

    as previous commit

--- through_genes.R
+++ through_genes.R
@@ -3,17 +3,6 @@ atr_genes <-  cGeneTable %>%
 atr_genes$tStart <- atr_genes$tEnd <- atr_genes$tStrand <- atr_genes$chr <- rep(NA, nrow(atr_genes))
 scf_order <- read.csv2('./atr_order.csv')
 
-# Делаем лист из data.frame'ов для каждой хромосомы 
-scf_order <- lapply(chr_order, function(i){
-  scf_order$i<-scf_order_data[scf_order_data$chr==i,]
-})
-names(scf_order) <- chr_order
-# Изначальная таблица больше не нужна
-rm(scf_order_data)
-
-# 
-
-
 for(i in 1:nrow(atr_genes)){
   
 }
